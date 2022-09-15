fabc6e792fbb735207ac9d42bd633343225ad82b
Than Zaw Toe <microempire37@gmail.com>
2022-09-15 21:32:17 +0630

    third test

--- test00.c
+++ test00.c
@@ -1,1 +1,7 @@
-hello test00
+#include <stdio.h>
+int main(){
+    int my_input;
+    printf("Hello World \n Please enter some Num:");
+    scanf("%d",&my_input);
+    printf("my input data is : %d",my_input);
+}
